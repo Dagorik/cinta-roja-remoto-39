9a14aa61427d8358efef6351628f8b664986e5c5
dagorik <dagorik@gmail.com>
2020-03-23 21:53:29 -0600

    repaso

---
+++ repaso/main.js
@@ -0,0 +1,24 @@
+// Tipo de dato string
+var string = "Hola batch remoto";
+var stringDos = 'A';
+
+// Tipo de dato number
+var number = 49;
+var numberDos = 43.23;
+
+
+// Tipo de dato booleano
+var booleano = true;
+var booleanoDos = false;
+
+
+// console.log(2 === "2");
+
+
+// El if solo valida booleanos
+var edad = 20;
+if (edad >= 18) {
+    console.log('Entre al IF');
+} else {
+    console.log('Entre al else');
+}
