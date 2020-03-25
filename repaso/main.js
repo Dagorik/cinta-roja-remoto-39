a165c522dbf942f10cef7adf7d0ade100a390876
dagorik <dagorik@gmail.com>
2020-03-24 20:10:37 -0600

    repaso de funciones

--- repaso/main.js
+++ repaso/main.js
@@ -1,3 +1,4 @@
+console.log('Iniciando el archivo');
 // Tipo de dato string
 var string = "Hola batch remoto";
 var stringDos = 'A';
@@ -20,6 +21,30 @@ var booleanoDos = false;
 //     console.log('Entre al else');
 // }
 
-for(var i = 0; i<99; i++){
-    console.log('Se ejecuto una vez ' + i);
+// for(var i = 0; i<99; i++){
+//     console.log('Se ejecuto una vez ' + i);
+// }
+
+// RETURN
+// El RETURN devulve un valor especifico desde una función y rompe la ejecición.
+
+// La definición de mi función
+function multiplicacion(num1, num2) {
+    let result = num1 * num2;
+    return result
 }
+
+const mult = multiplicacion(10,5);
+// console.log('El resultado es: '+ mult);
+
+function isPasswordValid(password) {
+    if (password !== 'passwordseguro') {
+        return 'password incorrecto' // El return tambien es un break para al funcion
+    }
+    // Doy por echo que el passoword es correcto
+    console.log('Se esta ejecutando la funcion');
+    return 'login'
+}
+
+const result = isPasswordValid('passwordseguro');
+console.log(result);
