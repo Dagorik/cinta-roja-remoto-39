04ef6ca666bb392e81bb90f9eeef7ca5020c9547
dagorik <dagorik@gmail.com>
2020-03-24 19:17:31 -0600

    create for

--- repaso/main.js
+++ repaso/main.js
@@ -13,9 +13,13 @@ var booleanoDos = false;
 
 
 // El if solo valida booleanos
-var edad = 20;
-if (edad >= 18) {
-    console.log('Entre al IF');
-} else {
-    console.log('Entre al else');
+// var edad = 20;
+// if (edad >= 18) {
+//     console.log('Entre al IF');
+// } else {
+//     console.log('Entre al else');
+// }
+
+for(var i = 0; i<99; i++){
+    console.log('Se ejecuto una vez ' + i);
 }
