4b118a0220be4fed6597c2ea893a277eef223634
dagorik <dagorik@gmail.com>
2020-03-27 20:00:02 -0600

    asyncronizidad

---
+++ jsasync/main.js
@@ -0,0 +1,19 @@
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+// console.log(1);
+// setTimeout(() => {
+//     console.log(2);
+// }, 3000);
+// setTimeout(() => {
+//     console.log(3);
+// }, 1000);
+// console.log(4);
+
+console.log(1);
+setTimeout(() => {
+    console.log(2);
+}, 1000);
+for(let i = 0; i<9999999999; i++ ); // ---> Es mi cuello de botella
+console.log(4);
