0c97817185c1445c8b72782cb40d15ddd8493096
dagorik <dagorik@gmail.com>
2020-03-30 21:55:47 -0600

    peticion a swapi

---
+++ peticiones/swapi.js
@@ -0,0 +1,10 @@
+// Mandar a llamar la biblioteca request (import)
+const request = require('request');
+
+const URL = 'https://swapi.co/api/people/20';
+request.get(URL, (error, response, body) => {
+    console.log(error);
+    console.log(response.statusCode);
+    const json = JSON.parse(body);
+    console.log(json.name);
+});
