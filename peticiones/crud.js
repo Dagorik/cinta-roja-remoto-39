40e7742756bbb1ac3864202b9b56179a71ea2390
dagorik <dagorik@gmail.com>
2020-04-01 21:48:04 -0600

    calback hell

--- peticiones/crud.js
+++ peticiones/crud.js
@@ -40,6 +40,56 @@ function updateIsAliveByAuthorId(idAuthor, isAlive) {
 
 updateIsAliveByAuthorId(4161, true);
 
+function todoEnUno(nombre, apellidos, biografia, genero, edad){
+    const URL = 'https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/';
+    const body = {
+        name: nombre,
+        last_name: apellidos,
+        nacionalidad: "MX",
+        biography: biografia,
+        gender: genero,
+        age: edad
+    };
+
+    const obj = { url: URL, form: body  };
+    request.post(obj, (err, response, body) => {
+        console.log(err);
+        console.log(response.statusCode);
+        const respuesta = JSON.parse(body);
+        console.log(respuesta);
+
+        //PATCH
+        const URL = `https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/${respuesta.id}/`;
+        const body = {
+            is_alive: true
+        };
+        const obj = { url: URL, form: body };
+        request.patch(obj, (err, response, body) => {
+            if (response.statusCode === 200) {
+                const respuesta = JSON.parse(body);
+                console.log(respuesta);
+                
+                //GET
+                const URL = 'https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/'+respuesta.id;
+                request.get(URL, (err, response, body  ) => {
+                    const respuesta = JSON.parse(body);
+                    console.log(respuesta);
+
+                    //DELETE
+                    const URL = 'https://goodreads-devf-aaron.herokuapp.com/api/v1/authors/'+respuesta.id;
+                    request.delete(URL, (err, response, body) => {
+                        if (response.statusCode === 204) {
+                            console.log('Se borro el usuario');
+                        }
+                    });
+                })
+            } else {
+                console.log('Error en la petición');
+            }
+        });
+    });
+}
+
 function callbackHell() {
     //CODE
     request.post(URL, () => {
@@ -52,9 +102,9 @@ function callbackHell() {
                     //CODE
                     request.get(URL, () => {
                         //CODE
-                    });  
+                    });
                 });
             });
         });
-    });
+    }); // ---> Bigotones tristes
 }
