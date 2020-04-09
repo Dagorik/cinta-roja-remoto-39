917076906d31e6ae4eb4dfb50e1ba762cfdab1f5
dagorik <dagorik@gmail.com>
2020-04-08 22:46:10 -0500

    cread and read superheroes

--- backend-mongo/app.js
+++ backend-mongo/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Superheroe = require('./models');
+const { Superheroe } = require('./models');
 
 const app = express();
 
@@ -9,6 +9,51 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send({ mensaje: 'Hola desde express' });
 });
+
+// Post == CREAR
+app.post('/heroe', (req, res) => {
+    console.log(req.body);
+    const heroe = {
+        nombre: req.body.nombre,
+        edad: req.body.edad,
+        superNovie: req.body.superNovie,
+        identidad: req.body.identidad,
+        debilidad: req.body.debilidad,
+        superPoderes: req.body.superPoderes
+    };
+
+    const newSuperHeroe = Superheroe(heroe);
+    
+    newSuperHeroe.save((err, superHeroe) => {
+        if (err) res.status(400).send(err)
+        else res.status(201).send(superHeroe);
+    });
+});
+
+// TODOS LOS HEROES
+app.get('/heroes', (req, res) => {
+    Superheroe.find().exec()
+        .then((respuesta) => {
+            res.send(respuesta);
+        })
+        .catch((err) => {
+            res.status(400).send(err)
+        })
+});
+
+// Devolver un heroe por ID
+// http://localhost:3000/hereo/5e8e8f8415ce7d10300ea7a1
+app.get('/heroe/:id', (req, res) => {
+    const idHeroe = req.params.id;
+    Superheroe.findOne({ _id: idHeroe }).exec()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            res.status(400).send(err)
+        });
+});
+
 
 app.listen(3000, () => {
     console.log('Server on');
--- backend-mongo/models/index.js
+++ backend-mongo/models/index.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const Superheroe = require('./Superheroe');
 
-const URL_MONGO = 'mongodb+srv://usuario:qwerty12@cluster0-in5es.mongodb.net/test?retryWrites=true&w=majority'
+const URL_MONGO = 'mongodb+srv://nuevousuario:qweqwe123@cluster0-in5es.mongodb.net/test?retryWrites=true&w=majority'
 mongoose.connect(URL_MONGO, { useNewUrlParser: true }, (err) => {
     if (!err) console.log('Conexión exitosa')
     else console.log(err);
 });
 
-module.exports = Superheroe;
+module.exports = { Superheroe };
